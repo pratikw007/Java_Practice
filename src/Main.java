07f5b9df6bffce3a46ac111c3748d232aa36ab30
Pratik Waghmare <waghmarepratik287@gmail.com>
2022-09-12 14:50:15 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //our first program
+//        System.out.println("Hello world");
+        //Primitives types
+        byte age = 30;
+        int phone = 1234567890;
+        long phone2 = 12345678900L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = false;
+    }
+}
---
+++ src/Strings.java
@@ -0,0 +1,21 @@
+public class Strings {
+    public static void main(String[] args) {
+        //Non-Primptive type
+//        String name = "Apu";
+//        String friend = new String("Akkku");
+//        System.out.println(name.length());
+       //Concatenates- comination of two String
+//        String name1 = "Pratik";
+//        String name2 = "Ashray";
+//        String  name3 = name1 + "  and  " +name2;
+//        System.out.println(name3);
+         //charAt
+//        String name = "Pratik";
+//        System.out.println(name.charAt(5));
+        //Length
+        String name = "Pratik";
+        System.out.println(name.length());
+
+        }
+
+}
